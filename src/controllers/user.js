e1955698fb3b650e9d30b36098791afdb0eed08e
fadhilahade <dhila.alkautsar@gmail.com>
2019-07-15 17:30:51 +0700

    add try catch

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,32 +1,50 @@
-const User = require('../models/user')
+const User = require("../models/user");
 
 module.exports = {
-    getAllUser: (req, res) => {
-        User.find({})
+  getAllUser: (req, res) => {
+    try {
+      User.find({})
         .then(data => res.send(data))
-        .catch(error => res.send({
-            message: "ups error happened, please call me to repair", error
-        }))
-    },
-    createUser: (req, res) => {
-        User.create({name: "Adit", age: 24, address:"Pasir Muncang Jaya"})
-        .then(data => res.send(data))
-        .catch(error => res.send(error))
-    },
-
-    deleteUser: (req, res) => {
-        User.remove({
-            name: "ucan"
-        })
-        .then(data => res.send(data))
-        .catch(error => res.send(error))
-    },
-
-    updateUser: (req, res) => {
-        User.findOneAndUpdate({name: "Adit"}, {age: 30}, (err, result) => {
-            if(err) throw err
-            res.send(result)
-        })
-
+        .catch(error =>
+          res.send({
+            message: "ups error happened, please call me to repair",
+            error
+          })
+        );
+    } catch (error) {
+      console.log(error);
     }
-}
+  },
+  createUser: (req, res) => {
+    try {
+      User.create({ name: "Adit", age: 24, address: "Pasir Muncang Jaya" })
+        .then(data => res.send(data))
+        .catch(error => res.send(error));
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  deleteUser: (req, res) => {
+    try {
+      User.remove({
+        name: "ucan"
+      })
+        .then(data => res.send(data))
+        .catch(error => res.send(error));
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  updateUser: (req, res) => {
+    try {
+      User.findOneAndUpdate({ name: "Adit" }, { age: 30 }, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
