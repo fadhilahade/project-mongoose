e00582f0723da8a38835b1803a16db4a2facf887
fadhilahade <dhila.alkautsar@gmail.com>
2019-07-16 12:13:59 +0700

    add mongodb-atlas

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
-mongoose.connect(`mongodb://localhost/${process.env.MONGO_DATABASE}`, {
+
+var mongodbConnection = process.env.MONGODB_ATLAS || `mongodb://localhost/${process.env.MONGO_DATABASE}`
+mongoose.connect( mongodbConnection, {
     useNewUrlParser: true
+})
+.catch(error => {
+    console.log("error happened when reaching mongodb connection", error);
+    
 })
 
 const db = mongoose.connection
